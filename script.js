4ef1248f6d609658ced3e5dab8cfda71df70f072
Mengxi He <mengxihex@gmail.com>
2022-04-03 02:35:40 +0200

    did two functions

--- script.js
+++ script.js
@@ -1,5 +1,42 @@
-console.log("hello")
+
 
 const computerPlay = () => {
     const options = ['Rock', 'Paper', 'Scissors']
+    const choice = options[Math.floor(Math.random()*options.length)];
+    return choice;
+}
+
+const play = (playerSelection, computerSelection) => {
+
+    playerSelection.toLowerCase();
+    let playerS = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1);
+    if (playerS === computerSelection){
+        console.log('its a tie!')
+    }
+    else if(playerS == 'Rock'){
+        if(computerPlay == 'Paper'){
+            console.log('You Lose! Paper beats Rock')
+        }
+        else{
+            console.log('You Won! Rock beats Scissors')
+        }
+    }
+
+    else if(playerS == 'Scissors'){
+        if(computerPlay == 'Rock'){
+            console.log('You Lose! Rock beats Scissors')
+        }
+        else{
+            console.log('You Won! Scissors beats Paper')
+        }
+    }
+
+    else if(playerS == 'Paper'){
+        if(computerPlay == 'Scissors'){
+            console.log('You Lose, Scissors beats Paper')
+        }
+        else{
+            console.log('You Won! Paper beats Rock')
+        }
+    }
 }
