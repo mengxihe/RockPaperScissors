507b1be4aecf30f674037cf5a31022b70b570d96
Mengxi He <mengxihex@gmail.com>
2022-04-11 14:22:57 +0200

    have the basic mechanism working, need to clean up, add details and add a good ui

--- script.js
+++ script.js
@@ -44,32 +44,22 @@ const playRound = (playerSelection, computerSelection) => {
 
 
 
-const game =(e) =>{
+const game =() =>{
 
     let userScore = 0;
     let computerScore = 0;
 
-    // for (let i = 0; i < 5; i++){
-    //     playerSelection = e.target.className;
-    //     computerSelection = computerPlay();
-    //     result = playRound(playerSelection, computerSelection);
-    //     console.log(result);
-    //     if (result.includes('Won')){
-    //         userScore += 1;
-    //     }
-    //     else if (result.includes('Lose')){
-    //         computerScore += 1;
-    //     }
-    // }
-    playerSelection = e.target.className;
-    computerSelection = computerPlay();
-    result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.includes('Won')){
-        userScore += 1;
-    }
-    else if (result.includes('Lose')){
-        computerScore += 1;
+    for (let i = 0; i < 5; i++){
+        playerSelection = window.prompt('please input your selection');
+        computerSelection = computerPlay();
+        result = playRound(playerSelection, computerSelection);
+        console.log(result);
+        if (result.includes('Won')){
+            userScore += 1;
+        }
+        else if (result.includes('Lose')){
+            computerScore += 1;
+        }
     }
 
     if (userScore > computerScore){
@@ -85,31 +75,47 @@ const game =(e) =>{
 }
 
 function play(e){
-    let userScore = 0;
-    let computerScore = 0;
+    // let userScore = 0;
+    // let computerScore = 0;
     let playerSelection = e.target.className;
     let computerSelection = computerPlay();
     let result = playRound(playerSelection, computerSelection);
-    
     const container = document.querySelector('#container');
     container.textContent = result;
     
-    if (result.includes('Won')){
-        userScore += 1;
-    }
-    else if (result.includes('Lose')){
-        computerScore += 1;
-    }
+    if (!gameOver){
+        if (result.includes('Won')){
+            userScore += 1;
+            user.textContent = `userScore: ${userScore}`;
+            if(userScore === limit){
+                gameOver =true;
+            }
+        }
 
-    const user = document.querySelector('#userScore');
-    user.textContent = `userScore: ${userScore}`;
-    const computer = document.querySelector('#computerScore');
-    computer.textContent = `computerScore: ${computerScore}`;
+        else if (result.includes('Lose')){
+            computerScore += 1;
+            computer.textContent = `computerScore: ${computerScore}`;
+            if(computerScore === limit){
+                gameOver = true;
+            }
+        }
+    }
+    else{
+        container.textContent = 'Game Over!';
+    }
     // game(e);
 }
 
+let userScore = 0;
+let computerScore = 0;
+let limit = 5;
+let gameOver = false;
+const user = document.querySelector('#userScore');
+const computer = document.querySelector('#computerScore');
+user.textContent = `userScore: ${userScore}`;
+computer.textContent = `computerScore: ${computerScore}`;
+const btn = [...document.querySelectorAll('#btn')];
 
-
-const btn = document.querySelectorAll('#btn');
 btn.forEach(btn => btn.addEventListener('click', play));
+
 
